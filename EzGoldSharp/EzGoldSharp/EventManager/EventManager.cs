706b15055a26919a88825ee649b5e93cbfafb36f
beminee <emirdota2@gmail.com>
2017-04-16 08:47:59 +0300

    wat
    wat

--- EzGoldSharp/EzGoldSharp/EventManager/EventManager.cs
+++ EzGoldSharp/EzGoldSharp/EventManager/EventManager.cs
@@ -32,7 +32,7 @@ namespace EzGoldSharp.EventManager
 
             Game.OnUpdate += EzGoldSharp.Game_OnUpdate;
             Game.OnUpdate += Updater.Update;
-            Drawing.OnDraw += Draws.Drawing;
+            Drawing.OnDraw += Draws.Drawing; // wat?
 
             Orbwalking.Load();
 
