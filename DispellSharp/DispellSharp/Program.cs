e56def7e89219211393033b608de23343ec27ba2
beminee <emirdota2@gmail.com>
2018-11-26 06:41:17 +0300

    Added suggestions

--- DispellSharp/DispellSharp/Program.cs
+++ DispellSharp/DispellSharp/Program.cs
@@ -42,6 +42,8 @@ namespace DispellSharp
         private item_manta Manta { get; set; }
         [ItemBinding]
         private item_nullifier Nullifier { get; set; }
+        [ItemBinding]
+        private item_lotus_orb LotusOrb { get; set; }
 
         [ImportingConstructor]
         public Program(
@@ -83,17 +85,31 @@ namespace DispellSharp
             {
                 if (Manta != null && Manta.CanBeCasted &&
                     (this.Owner.IsSilenced() || this.Owner.HasModifier("modifier_silencer_last_word")) &&
-                    this.Config.MantaSilences.Value)
+                    this.Config.Silences.Value)
                 {
                     Manta.UseAbility();
                     await Task.Delay(Manta.GetCastDelay(), token);
                 }
+                else if (LotusOrb != null && LotusOrb.CanBeCasted &&
+                         (this.Owner.IsSilenced() || this.Owner.HasModifier("modifier_silencer_last_word")) &&
+                         this.Config.Silences.Value)
+                {
+                    LotusOrb.UseAbility(this.Owner);
+                    await Task.Delay(LotusOrb.GetCastDelay(), token);
+                }
 
                 if (Manta != null && Manta.CanBeCasted && this.Owner.HasModifier("modifier_item_dustofappearance") &&
-                    this.Config.MantaDust.Value)
+                    this.Config.Dust.Value)
                 {
                     Manta.UseAbility();
                     await Task.Delay(Manta.GetCastDelay(), token);
+                }
+                else if (LotusOrb != null && LotusOrb.CanBeCasted &&
+                         this.Owner.HasModifier("modifier_item_dustofappearance") &&
+                         this.Config.Dust.Value)
+                {
+                    LotusOrb.UseAbility(this.Owner);
+                    await Task.Delay(LotusOrb.GetCastDelay(), token);
                 }
 
                 var enemiesAround = EntityManager<Hero>.Entities
@@ -105,6 +121,12 @@ namespace DispellSharp
                     if (enemy != null && !enemy.IsMagicImmune() && Nullifier.CanBeCasted)
                     {
                         if (enemy.IsEthereal() && this.Config.EnemyCleanseToggler.Value.IsEnabled("item_ghost"))
+                        {
+                            this.Nullifier.UseAbility(enemy);
+                            await Task.Delay(Nullifier.GetCastDelay(), token);
+                        }
+                        else if (enemy.HasModifier("modifier_item_aeon_disk_buff") &&
+                                 this.Config.EnemyCleanseToggler.Value.IsEnabled("item_aeon_disk"))
                         {
                             this.Nullifier.UseAbility(enemy);
                             await Task.Delay(Nullifier.GetCastDelay(), token);
@@ -141,8 +163,8 @@ namespace DispellSharp
 
         public MenuFactory Menu { get; }
 
-        public MenuItem<bool> MantaSilences { get; set; }
-        public MenuItem<bool> MantaDust { get; set; }
+        public MenuItem<bool> Silences { get; set; }
+        public MenuItem<bool> Dust { get; set; }
 
         public MenuItem<AbilityToggler> EnemyCleanseToggler { get; set; }
 
@@ -152,15 +174,16 @@ namespace DispellSharp
             {
                 {"item_ghost", true},
                 {"omniknight_guardian_angel", true},
-                {"item_cyclone", true}
+                {"item_cyclone", true},
+                {"item_aeon_disk", true }
             };
 
             this.Menu = MenuFactory.Create("DispellSharp");
-            this.MantaSilences = this.Menu.Item("Dispell Silence with Manta", true);
-            this.MantaSilences.Item.Tooltip =
+            this.Silences = this.Menu.Item("Dispell Silence with Manta", true);
+            this.Silences.Item.Tooltip =
                 "Setting this to false will disable dispelling silence effects with Manta.";
-            this.MantaDust = this.Menu.Item("Dispell Dust with Manta", true);
-            this.MantaDust.Item.Tooltip = "Setting this to false will disable dispelling dust effect with Manta.";
+            this.Dust = this.Menu.Item("Dispell Dust with Manta", true);
+            this.Dust.Item.Tooltip = "Setting this to false will disable dispelling dust effect with Manta.";
             this.EnemyCleanseToggler = this.Menu.Item("Enemy Cleanse Toggler", new AbilityToggler(enemyCleanse));
         }
 
